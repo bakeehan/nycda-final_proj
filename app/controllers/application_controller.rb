a886977413f5f539cc8b3c4efabc0ed327876b24
Bridget Keehan <ba.keehan@gmail.com>
2018-07-09 11:14:05 -0400

    users have avatar option as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:displayname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:displayname, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:displayname, :avatar])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_143020) do
+ActiveRecord::Schema.define(version: 2018_07_09_150246) do
 
   create_table "achievements", force: :cascade do |t|
     t.integer "activity_id"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2018_07_09_143020) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
